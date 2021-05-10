6512e2dd5d5c0ad88b2550696c09d8db42a58cf0
Kat Hurdley <katherine.hurdley@googlemail.com>
2021-05-10 22:25:53 +0100

    working on heart component

--- src/components/FeaturedCharacter.js
+++ src/components/FeaturedCharacter.js
@@ -5,11 +5,20 @@ import "./FeatCharacter.css";
 
 function FeaturedCharacter(props) {
 
+const [friendship, setFriendship] = useState(props.featVillager.hearts)
 
     function writeList(list){
         console.log(list)
         let faves = list.join(', ');
         return faves;
+    }
+
+function generateHearts(n){
+      let friendship =[];
+      for (let i = 0; i<n;i++){
+        friendship.push(<img width = '30px' src ="https://e7.pngegg.com/pngimages/616/424/png-clipart-health-video-game-pixel-art-bar-health-game-rectangle-thumbnail.png"></img>)
+      }
+      return friendship
     }
 
     return (
@@ -22,6 +31,7 @@ function FeaturedCharacter(props) {
                   </td>
                   <td>
                     <h2>{props.featVillager.name}</h2>
+                    <li>Friendship Level: {props.featVillager.hearts && generateHearts(props.featVillager.hearts)}</li>
                     <li>Birthday: {props.featVillager.birthday}</li>
                     <li>Favourite Gifts: {props.featVillager.favGifts && writeList(props.featVillager.favGifts)}</li>
                   </td>
